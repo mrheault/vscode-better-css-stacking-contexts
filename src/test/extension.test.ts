a3857eba849be1a97fce2b92f060798bda052a43
Mike Rheault <mike.rheault@ef.com>
2024-07-21 09:30:03 -0400

    simple extension test for now

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,23 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as path from 'path';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+  vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+  test('Extension should be present', () => {
+    assert.ok(
+      vscode.extensions.getExtension(
+        'undefined_publisher.vscode-better-css-stacking-contexts',
+      ),
+    );
+  });
+
+  test('Extension should activate', async () => {
+    const extension = vscode.extensions.getExtension(
+      'undefined_publisher.vscode-better-css-stacking-contexts',
+    );
+    await extension?.activate();
+    assert.ok(extension?.isActive);
+  });
 });
